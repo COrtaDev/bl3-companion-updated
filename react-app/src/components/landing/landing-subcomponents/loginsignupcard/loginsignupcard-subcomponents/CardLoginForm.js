10edcbd2108463f3be1ba9945942ab3e87a8d893
Conrad <conradorta@gmail.com>
2021-02-02 13:06:32 -0500

    Added Form TODO: setup JavaScript to toggle between forms and TODO: implement JavaScript to make background dynamic... Good Luck Conrad

--- react-app/src/components/landing/landing-subcomponents/loginsignupcard/loginsignupcard-subcomponents/CardLoginForm.js
+++ react-app/src/components/landing/landing-subcomponents/loginsignupcard/loginsignupcard-subcomponents/CardLoginForm.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
 
 const CardLoginForm = () => {
   //TODO:
   //need to define the Login Form here, use the LoginForm in 'auth/' as reference
+  return (
+    <form>
+      <div className={"field"}>
+        <p className={"control has-icons-left"}>
+          <input className={"input"} type={"email"} placeholder={"Email"} />
+          <span className={"icon is-small is-left"}>
+            <FontAwesomeIcon icon={faEnvelope} />
+          </span>
+        </p>
+      </div>
+      <div className={"field"}>
+        <p className={"control has-icons-left"}>
+          <input className={"input"} type={"password"} placeholder={"Password"} />
+          <span className={"icon is-small is-left"}>
+            <FontAwesomeIcon icon={faLock} />
+          </span>
+        </p>
+      </div>
+      <div className={"field is-grouped"}>
+        <div className={"control"}>
+          <button className={"button is-primary"}>Login With Demo User</button>
+        </div>
+        <div className={"control"}>
+          <button className={"button is-link"}>Login</button>
+        </div>
+      </div>
+    </form>
+  );
 
 }
 
--- react-app/src/components/landing/landing-subcomponents/loginsignupcard/loginsignupcard-subcomponents/LoginSignUpCardMedia.js
+++ react-app/src/components/landing/landing-subcomponents/loginsignupcard/loginsignupcard-subcomponents/LoginSignUpCardMedia.js
@@ -1,12 +1,24 @@
 import React from 'react';
 //TODO:
 //need to import the LOGIN & SIGNUP forms
+import CardLoginForm from './CardLoginForm';
+import CardSignUpForm from './CardSignUpForm';
 
 const LoginSignUpCardMedia = () => {
   //TODO:
   //need define the MEDIA component here...
   //needs to render the LOGIN or SIGNUP form depending on the TAB selected...
-
+  return (
+    <article className={"media"}>
+      <div className={"media-content"}>
+        <div className={"content"}>
+          {/* Welcome back traveler! Please login to see your loot! */}
+        </div>
+        <CardLoginForm />
+        {/* <CardSignUpForm /> */}
+      </div>
+    </article>
+  );
 }
 
 export default LoginSignUpCardMedia;
