b3e6a67a5d2f3e01003be307a8a8f22cd4651b5d
Conrad <conradorta@gmail.com>
2021-02-04 13:19:59 -0500

    Added all models over from the previous version of the project.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,6 @@ from .db import db
 from .user import User
 from .loot_item import Loot_Item
 from .loot_drop import Loot_Drop
+from .comment import Comment
+from .follow import Follow
+from .like import Like
