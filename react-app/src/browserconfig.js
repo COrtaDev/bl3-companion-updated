e90a4dd9785a6e7f5455ce024e7778dbfa9f5172
Conrad <conradorta@gmail.com>
2021-02-15 18:55:20 -0500

    start of defining browser config

---
+++ react-app/src/browserconfig.js
@@ -0,0 +1,12 @@
+//* This is our centralized route config...
+// A route config is just data. React is great at mapping
+// data into components, and <Route> is a component.
+
+// Our route config is just an array of logical "routes"
+// with `path` and `component` props, ordered the same
+// way you'd do inside a `<Switch>`.
+
+
+const routes = [];
+
+export default routes;
