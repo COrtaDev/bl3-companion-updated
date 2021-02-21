83f5836455096b839fd133d0a7eb2d7bb6213295
Conrad <conradorta@gmail.com>
2021-02-21 16:51:22 -0500

    Documentation and formatting changes

--- react-app/src/components/main/Main.js
+++ react-app/src/components/main/Main.js
@@ -7,18 +7,34 @@ import Feed from "./main-subcomponents/Feed";
 const Main = () => {
   return (
     <>
-      <nav className={"level is-align-items-start"}>
-        <LeftSidebarMenu />
-
-        <div className={"column is-half"}>
-          <div className={"container"}>
-            <Header />
-            <Feed />
+      <div className={"columns is-vcentered is-align-items-flex-start"}>
+        <div className={"column is-one-fifth"}>
+          <div className={"level-right"}>
+            <LeftSidebarMenu />
           </div>
         </div>
 
-        <RightSidebarMenu />
-      </nav>
+        <div className={"column is-two-thirds"}>
+          <div className={"columns"}>
+            <div
+              className={
+                "column is-two-thirds-fullhd is-two-thirds-widescreen is-two-thirds-desktop is-two-thirds-tablet is-two-thirds-mobile"
+              }
+            >
+              <div className={"level is-flex-direction-column"}>
+                <Header />
+                <Feed />
+              </div>
+            </div>
+
+            <div className={"column"}>
+              <div className={"level-left"}>
+                <RightSidebarMenu />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
