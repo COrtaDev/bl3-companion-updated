03bcbe44b591b6a7468c6b7785bcefb2c9f0a961
Conrad <conradorta@gmail.com>
2021-02-22 21:18:49 -0500

    Defined Content component, which is everything to the right of the sidenav panel component

--- react-app/src/components/main/Main.js
+++ react-app/src/components/main/Main.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import LeftSidebarMenu from "./main-subcomponents/LeftSidebarMenu";
-import RightSidebarMenu from "./main-subcomponents/RightSidebarMenu";
-import Header from "./main-subcomponents/Header";
-import Feed from "./main-subcomponents/Feed";
 import { sidebarRoutes } from "../../browserconfig";
-import SubHeader from "./main-subcomponents/SubHeader";
+
+import Content from "./Content";
 
 const Main = () => {
+  //TODO: We need to render a sibling component that encapsulates everything nested
+  //*inside of the main tags... This is done to allow sidenav route config to function
+  //as described in the documentation on the React Router site...
   return (
     <>
       <div className={"is-flex is-flex-direction-row"}>
@@ -17,21 +18,7 @@ const Main = () => {
         >
           <LeftSidebarMenu />
         </section>
-        <main
-          className={"is-flex is-flex-direction-row is-justify-content-center"}
-        >
-          <section
-            // style={{ minWidth: "600px" }}
-            className={"is-flex is-flex-direction-column is-align-items-center"}
-          >
-            <Header />
-            <SubHeader />
-            <Feed />
-          </section>
-          <section style={{ borderLeft: "1px solid" }} className={"is-flex"}>
-            <RightSidebarMenu />
-          </section>
-        </main>
+        <Content />
       </div>
     </>
   );
--- react-app/src/components/main/main-subcomponents/SubHeader.js
+++ react-app/src/components/main/main-subcomponents/SubHeader.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 const SubHeader = () => {
+  // TODO: this will most likely be a media card, with options to render
+  //* the Create Loot Drop Component...
+  //? if we are on a route like following or likes or comments etc
+  //?we may end up returning null from this component, which is fine...
+  //!OR we will need to render tabs for following etc but will need to be
+  //!developed more thoughly at a later time...
   return (
     <>
       <section style={{ minWidth: "600px" }} className={"section"}>
