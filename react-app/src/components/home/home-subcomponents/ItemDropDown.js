78560f7dd6c1526a5dfcc569ca3ea90608c31ca8
Conrad <conradorta@gmail.com>
2021-03-02 13:13:41 -0500

    small refector of ItemDropDown to make the nesting more readable

--- react-app/src/components/home/home-subcomponents/ItemDropDown.js
+++ react-app/src/components/home/home-subcomponents/ItemDropDown.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { loot } from "../../../services/loot";
 
 const ItemDropDown = () => {
   const [active, setActive] = useState("");
@@ -8,23 +9,34 @@ const ItemDropDown = () => {
   const openMenu = (e) => (!active ? setActive("is-active") : setActive(""));
 
   return (
-    <div id={"item-dropdown"} className={`dropdown ${active}`}>
-      <div id={"item-dropdown"} className={"dropdown-trigger"}>
-        <button
-          id={"item-dropdown-button"}
-          className={"button is-fullwidth"}
-          onClick={openMenu}
-        >
-          <span>DropDown Button</span>
-          <span className={"icon is-small"}>
-            <FontAwesomeIcon icon={faAngleDown} />
-          </span>
-        </button>
-      </div>
-      <div id={"dropdown-menu"} className={"dropdown-menu"} role={"menu"}>
-        <div className={"dropdown-content"}>
-          
+    <div className={"field has-addons has-addons-centered"}>
+      <div className={"control is-expanded"}>
+        <div id={"item-dropdown"} className={`dropdown ${active}`}>
+          <div id={"item-dropdown"} className={"dropdown-trigger"}>
+            <button
+              id={"item-dropdown-button"}
+              className={"button is-fullwidth"}
+              onClick={openMenu}
+            >
+              <span>DropDown Button</span>
+              <span className={"icon is-small"}>
+                <FontAwesomeIcon icon={faAngleDown} />
+              </span>
+            </button>
+          </div>
+          <div id={"dropdown-menu"} className={"dropdown-menu"} role={"menu"}>
+            <div className={"dropdown-content"}>
+              {loot.map((item) => (
+                <div className={"dropdown-item"}>{item.name}</div>
+              ))}
+            </div>
+          </div>
         </div>
+      </div>
+      <div className={"control"}>
+        <button type={"submit"} className={"button"}>
+          Choose Item
+        </button>
       </div>
     </div>
   );
--- react-app/src/components/home/home-subcomponents/LootDropForm.js
+++ react-app/src/components/home/home-subcomponents/LootDropForm.js
@@ -4,33 +4,17 @@ import ItemDropDown from "./ItemDropDown";
 const LootDropForm = ({ hideForm }) => {
   return (
     <div id={"loot-drop-card"} className={`card is-shadowless animatedCard`}>
-      {/* <div className={"is-flex is-justify-content-flex-end m-3"}>
-        <div className={"media-right"}>
-          <button className={"delete is-medium"} onClick={hideForm}></button>
-        </div>
-      </div> */}
-
       <div className={"card-content"}>
-        <div className={"field has-addons has-addons-centered"}>
-          <div className={"control is-expanded"}>
-            <ItemDropDown />
-            {/* <div className={"select is-fullwidth"}>
-              <select name={"item"}>
-                <option value={"item-01"}>Item 1</option>
-                <option value={"item-02"}>Item 2</option>
-                <option value={"item-03"}>Item 3</option>
-                <option value={"item-04"}>Item 4</option>
-                <option value={"item-05"}>Item 5</option>
-                <option value={"item-06"}>Item 6</option>
-              </select>
-            </div> */}
-          </div>
-          <div className={"control"}>
+        {/* <div className={"field has-addons has-addons-centered"}> */}
+        {/* <div className={"control is-expanded"}> */}
+        <ItemDropDown />
+        {/* </div> */}
+        {/* <div className={"control"}>
             <button type={"submit"} className={"button"}>
               Choose Item
             </button>
-          </div>
-        </div>
+          </div> */}
+        {/* </div> */}
       </div>
 
       <footer className={"card-footer"}>
